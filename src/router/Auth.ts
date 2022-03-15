eecf26f32caeedd6b1ab2713a8e951cf635919b1
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-03-15 13:28:14 +0200

    D158-1

--- src/router/Auth.ts
+++ src/router/Auth.ts
@@ -17,13 +17,25 @@ router.post('/signup',async (req:Request, res:Response)=>{
         const token = createCookieUserId(newuser.id);
         res.cookie('__SER', token, {httpOnly: true})
         res.status(200).json(newuser)
-        console.log('a7a')
     }catch(err){
-        console.log(err)
+        res.status(400).json(err)
     }
 })
 
 // login
+router.post('/signin', async(req:Request, res:Response)=>{
+    try{
+        const user = await User.findOne({email: req.body.email});
+        if(!user) res.status(400).json('user not found')
 
+        const password = await bcrypt.compare(req.body.password, user.password)
+        if(!password) res.status(400).json("wrong password")
+
+        
+        res.status(200).json('Welcome')
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
 
 export default router;
---
+++ src/router/User.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import User from '../model/User';
+import bcrypt from 'bcrypt';
+const router = express.Router();
+
+// Get User
+
+// Update data
+
+// Delete data
+
+export default router;
