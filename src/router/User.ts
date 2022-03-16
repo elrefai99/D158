cfdc019a83616bf07c0ee261a42556d4dd08761f
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-03-16 16:27:19 +0200

    D158-1 Last one

--- src/router/User.ts
+++ src/router/User.ts
@@ -1,10 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import User from '../model/User';
 import bcrypt from 'bcrypt';
 const router = express.Router();
 
 // Get User
-
+router.get('/:id', async (req:Request, res:Response) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(req.params.id);
+    }catch(err){
+        res.status(400).json(err)
+    }
+})
 // Update data
 
 // Delete data
